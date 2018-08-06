733fdea08857a97bc2ad75762bebd7d2ff2ab675
jiwang <jingkui.wang@imp.ac.at>
2018-08-06 13:50:05 +0200

    backup before change f2min_profile function

--- R/identifiability_analysis.R
+++ R/identifiability_analysis.R
@@ -12,55 +12,16 @@ source("R/set_bounds_initialize_values.R", local = TRUE)
 
 Identifiablity.analysis.gamma.all.models = function(param.fits.results, GeneDataSet)
 {
-  zt = unlist(GeneDataSet$zt)
-  R.m = unlist(GeneDataSet$R.m) #R.m = unlist(T[gene.index, i.ex]) ## nb of reads for exon
-  R.s = unlist(GeneDataSet$R.s) #R.s = unlist(T[gene.index, i.int]) ## nb of reads for intron
-  L.m = GeneDataSet$L.m # L.m = T$length.mRNA[gene.index];
-  L.s = GeneDataSet$L.s  #L.s = T$length.premRNA[gene.index];
+  res.ident.all = c();
   
-  alpha.m = unlist(GeneDataSet$alpha.m)
-  alpha.s = unlist(GeneDataSet$alpha.s)
-  
-  outlier.m = unlist(GeneDataSet$outlier.m)
-  outlier.s = unlist(GeneDataSet$outlier.s)
-  
-  M = norm.RPKM(R.m, L.m)
-  S = norm.RPKM(R.s, L.s)
-  a = mean(M)/mean(S) # ratio between splicing rate and degratation rate
-  
-  res.iden.all = c();
   for(model in c(2:4))
   {
     cat('\t model ', model, '\n');
-    error.fit = param.fits.results[which(names(param.fits.results)==paste('error.m', model, sep=''))];
-    res.fit = param.fits.results[grep(paste('.m', model, sep=''), names(param.fits.results))];
-    res.fit = res.fit[-1];
     
-    bounds.gene = set.bounds.gene(GeneDataSet = GeneDataSet, model);
-    upper = bounds.gene$upper; 
-    lower = bounds.gene$lower;
+    id.gamma.spec.model = Identifiablity.analysis.gamma.each.model(param.opt = param.fits.results, GeneDataSet=GeneDataSet, model = model)
+    names(id.gamma.spec.model) = paste(names(id.gamma.spec.model), '.m', model, sep='');
     
-    if(model==2) res.fit = res.fit[1:6];
-    if(model==3) res.fit = res.fit[1:5];
-    if(model==4) res.fit = res.fit[1:8];
-    
-    res.nonident.analysis.gamma.spec.model = Identifiablity.analysis.gamma.each.model(error.opt=error.fit, 
-                                                                                      params.opt =res.fit, 
-                                                                                      lower = lower, 
-                                                                                      upper = upper, 
-                                                                                      R.m = R.m, 
-                                                                                      R.s = R.s, 
-                                                                                      L.m=L.m, 
-                                                                                      L.s = L.s, 
-                                                                                      alpha.m=alpha.m, 
-                                                                                      alpha.s=alpha.s, 
-                                                                                      outlier.m = outlier.m, 
-                                                                                      outlier.s = outlier.s, 
-                                                                                      model = model, 
-                                                                                      zt = zt);
-    
-    names(res.nonident.analysis.gamma.spec.model) = paste(names(res.nonident.analysis.gamma.spec.model), '.m', model, sep='');
-    res.iden.all = c(res.iden.all, res.nonident.analysis.gamma.spec.model);
+    res.iden.all = c(res.iden.all, id.gamma.spec.model);
   }
   
   return(res.iden.all)
@@ -68,34 +29,54 @@ Identifiablity.analysis.gamma.all.models = function(param.fits.results, GeneData
 }
 
 
-Identifiablity.analysis.gamma.each.model = function (error.opt, params.opt, lower, upper, R.m, R.s, L.m, L.s, alpha.m, alpha.s, model = 3, zt,
-                                                     outlier.m = c(), outlier.s = c(), PLOT.PL = FALSE, gene2plot='Per1') 
+###############################
+# model-wise in case checking gamma identifiability for particular model 
+###############################
+Identifiablity.analysis.gamma.each.model = function(param.opt, GeneDataSet, model = 4,
+                                                    quick.check.boundary = TRUE, profile.plotting = FALSE) 
 {
-  # error.opt=errors.fit[imin]; params.opt =res.fit;PLOT.PL = TRUE; gene2plot=gene.name;
-  #cat('FAST identifiability analysis for gamma \n');
-  if(length(outlier.m)>0) outlier.m = outlier.m[which(!is.na(outlier.m)==TRUE)];
-  if(length(outlier.s)>0) outlier.s = outlier.s[which(!is.na(outlier.s)==TRUE)];
-  params = params.opt;
-  gamma.opt = params[1];
-  #gamma.opt = log(2)/(24)
-  nb.test.iden = 20;
-  list.par = lseq(log(2)/24, log(2)/(10/60), length=nb.test.iden);
+  # param.opt = param.fits.results; model = 4; quick.check.boundary = TRUE;
+  if(quick.check.boundary){ nb.profile = 2;}
+  if(profile.plotting) {nb.profile = 10; }
+  
+  ## specify the boundaries for parameters in the same way as optimization_params.R
+  bounds.gene = set.bounds.gene(GeneDataSet = GeneDataSet, model);
+  upper = bounds.gene$upper; 
+  lower = bounds.gene$lower;
+  
+  list.gamma = lseq(lower[which(names(lower)=="gamma")], 
+                    upper[which(names(upper)=="gamma")], length=nb.profile);
   #list.par = unique(c(list.par, gamma.opt))
   #list.par = list.par[order(list.par)];
   #ii = which(list.par==gamma.opt);
   
-  #res.pl = rep(NA, Nfit.pl)
-  profiles = matrix(NA, nrow =length(list.par) , ncol=2);
+  profiles = matrix(NA, nrow =length(list.gamma) , ncol=2);
   colnames(profiles) = c('gamma', 'pls');
+  profiles[,1] = list.gamma;
   
-  profiles[,1] = list.par;
+  ## extracting the optimal parameters and error for specific model
+  index.error = which(names(param.opt)==paste('error.m', model, sep=''));
+  index.param = setdiff(grep(paste('.m', model, sep=''), names(param.opt)), c(index.error, grep("stderr", names(param.opt)))) 
+  error.fit = param.opt[index.error];
+  param.fit = param.opt[index.param];
+  
+  
   ptm <- proc.time()
   for(n in 1:nrow(profiles))
   {
-    opt.pl = optim(params, par.fixed = profiles[n, 1], index.fixed = 1, f2min.profile, 
-                   R.m = R.m, R.s = R.s, L.m=L.m, L.s = L.s, alpha.m=alpha.m, alpha.s=alpha.s, outlier.m = outlier.m, outlier.s = outlier.s, model = model, 
-                   zt = zt, method = 'L-BFGS-B', lower = lower, upper = upper)
-    profiles[n, 2] = opt.pl$value;
+    if(profiles[n, 1] == param.fit[1]){
+      profiles[n, 2] = error.fit;
+    }else{
+      param.start = param.fit; 
+      param.start[1] = profiles[n, 1];
+      #opt = optim(par.init, f2min, GeneDataSet = GeneDataSet, model = model, debug = debug, norm.params=norm.params,
+      #            method = 'L-BFGS-B', lower = bounds.g$lower, upper = bounds.g$upper, 
+      #            control = list(maxit=500,trace=0), hessian = FALSE)
+      opt.pl = optim(param.start, f2min.profile, GeneDataSet = GeneDataSet, which.to.fix =1,  model = model, 
+                    method = 'L-BFGS-B', lower = lower, upper = upper)
+      profiles[n, 2] = opt.pl$value;
+    }
+    
   }
   proc.time() - ptm;
   
@@ -109,17 +90,14 @@ Identifiablity.analysis.gamma.each.model = function (error.opt, params.opt, lowe
   ## the following threshod is used for the current code
   threshold3 = qchisq(p=0.68, df=1, lower.tail = TRUE) + error.opt;
   
-  if(PLOT.PL){ ## NOT Used here
-    #pdf.name = paste('/Users/jiwang/Degradation_Liver/Main_Code_Degradation/myplots/Total/Total_counts/identifiability_test_Real_Data/gamma_nonidentif_',
-    #                   gene2plot, '_m', model, '.pdf', sep=''); 
-    #pdf(pdf.name, width = 5 , height = 3);
+  if(profile.plotting){ ## NOT Used here
     #par(cex = 0.7, las = 1, mgp = c(2.0,0.5,0), mar = c(3,3,2,0.8)+0.1, tcl = -0.3);
     lims = range(c(error.opt, threshold1, threshold2, (threshold1+5)));
     plot(PL$gamma, PL$pls, type='l', col='darkblue', log='', lwd=1.5, xlab='gamma', ylab='-2log(PL)', ylim = lims, main = paste('m', model, sep='')); 
     points(PL$gamma, PL$pls, type='p', cex=0.8, col='darkblue'); 
     points(params[1], error.opt, col='darkred', cex=1.5, pch=8); 
     abline(h=threshold1, col='red', lwd=1.5, lty=2);abline(h=threshold2, col='red', lwd=1.5, lty=1);abline(h=threshold3, col='red', lwd=1.5, lty=1)  
-    #dev.off()
+    
   }
   
   bound.left = c(); bound.right = c();
