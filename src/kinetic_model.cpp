da3406f3f9a7be436aabaa6755adbdf9d8e1ddfc
jiwang <jingkui.wang@imp.ac.at>
2018-08-09 16:06:53 +0200

    first version of integration using Rcpp; but reaching similar speed

--- src/kinetic_model.cpp
+++ src/kinetic_model.cpp
@@ -1,30 +1,10 @@
 //[[Rcpp::depends(RcppEigen)]]
 //[[Rcpp::depends(RcppNumerical)]]
-//#include <Rcpp.h>
+#include <Rcpp.h>
 #include <math.h>
 #include <RcppNumerical.h>
 
 using namespace Rcpp;
-class Func
-{
-public:
-  virtual double operator()(const double& x) const = 0;
-  virtual void eval(double* x, const int n) const
-  {
-    for(int i = 0; i < n; i++)
-      x[i] = this->operator()(x[i]);
-  }
-  
-  virtual ~Func() {}
-};
-
-inline double integrate(
-    const Func& f, const double& lower, const double& upper,
-    double& err_est, int& err_code,
-    const int subdiv = 100, const double& eps_abs = 1e-8, const double& eps_rel = 1e-6,
-    const Rcpp Integrator<double>::QuadratureRule rule = Integrator<double>::GaussKronrod41
-)
-  
 
 /*
  * function to calculate s
@@ -45,6 +25,16 @@ NumericVector compute_s_beta(NumericVector t, double Min, double Amp, double pha
   return s;
   
 }
+
+double compute_s_t(double t, double Min, double Amp, double phase, double beta){
+
+  const double pi = 3.14159265358979323846;
+  double w = 2.0*pi/24.0;
+  double s = Min + Amp * pow(((1.0+cos(w*(t-phase)))/2.0), beta);
+  return s;
+
+}
+
 /*compute.s.beta = function(t = zt, Min = 1, Amp = 2, phase = 12, beta = 1)
  *{
  * w = 2*pi/24;
@@ -52,7 +42,31 @@ NumericVector compute_s_beta(NumericVector t, double Min, double Amp, double pha
  * return(s)
  *}
  */
-
+class Fun_to_Integrate: public Numer::Func{
+private:
+  double gamma;
+  double eps_gamma; 
+  double phase_gamma;
+  double Min;
+  double Amp;
+  double phase;
+  double beta;
+  
+public:
+  Fun_to_Integrate(double gamma_, double eps_gamma_, double phase_gamma_,
+                   double Min_, double Amp_, double phase_, double beta_) : 
+  gamma(gamma_), eps_gamma(eps_gamma_), phase_gamma(phase_gamma_), Min(Min_), Amp(Amp_), phase(phase_), beta(beta_){}
+  
+  double operator()(const double& t) const
+  {
+    const double pi = 3.14159265358979323846;
+    double w = 2.0*pi/24.0;
+    double Gamma = gamma * (t + eps_gamma/w * sin(w * (t - phase_gamma)));
+    double s = Min + Amp * pow(((1.0+cos(w*(t-phase)))/2.0), beta);
+    return exp(Gamma) * s;  
+  }
+  
+}; 
 
 /*
  * function to calculate m
@@ -69,20 +83,65 @@ NumericVector compute_m_beta(NumericVector t,
   int i;
   NumericVector m(n);
   
+  double lower = 0.0;
+  double upper;
+  double Tstable = 24*(ceil(log(2000)/gamma/24) + 1.0);
+  
+  Fun_to_Integrate f(gamma, eps_gamma, phase_gamma, Min, Amp, phase, beta);
+  double err_est;
+  int err_code;
+  double res_integrate;
+  double res_Gamma;
+  
   for(i = 0; i < n; i++){
+    upper = Tstable + t[i];
     
-    m(i) = 0.2 *  Min + Amp * pow(((1.0+cos(w*(t(i)-phase)))/2.0), beta);
+    res_integrate = Numer::integrate(f, lower, upper, err_est, err_code);
+    res_Gamma =  gamma * (upper + eps_gamma/w * sin(w * (upper - phase_gamma)));
+    //m(i) = 0.2 *  Min + Amp * pow(((1.0+cos(w*(t(i)-phase)))/2.0), beta);
+    m[i] = splicing_k * exp(-res_Gamma) * res_integrate ;
+    //m[i] = splicing.k*exp(-Gamma(t = Tstable+time, gamma = gamma, eps.gamma=eps.gamma, phase.gamma=phase.gamma)) *
+    //integrate(f2integrate, lower = 0, upper = Tstable+time, par = c(gamma, eps.gamma, phase.gamma, Min, Amp, phase, beta))$value
     
   }
   
   return m;
-  
 }
 
 /*
- * integration
+ *  Gamma = function(t = 0, gamma = log(2)/3, eps.gamma = 0.2, phase.gamma = 0)
+ {
+w = 2*pi/24;
+Gamma = gamma*(t + eps.gamma/w * sin(w*(t-phase.gamma)))
+#Gamma = gamma*(t+ eps.gamma/w * sin(w*(t-phase.gamma)))
+#Gamma = gamma*(t+ eps.gamma/w * sin(w*(t-phase.gamma)))
+#Gamma = gamma*t + amp.gamma/2*(t+ sin(w*(t-phase.gamma))/w)
+return(Gamma)
+
+*/
+
+double Gamma_copy(double t, double gamma, double eps_gamma, double phase_gamma){
+  
+  const double pi = 3.14159265358979323846;
+  double w = 2.0*pi/24.0;
+  double res_Gamma = gamma * (t + eps_gamma/w * sin(w * (t - phase_gamma)));
+  
+  return res_Gamma;
+}
+
+/*
+ * f2integrate = function(t, par)
+ {
+gamma = par[1]; 
+eps.gamma= par[2];
+phase.gamma= par[3]; 
+Min = par[4];
+Amp = par[5];
+phase = par[6];
+beta = par[7];
+return(exp(Gamma(t, gamma, eps.gamma, phase.gamma)) * compute.s.beta(t, Min, Amp, phase, beta))
+}
  */
-class func_to_integrate: public RcppNumerical::Func
 
 
 /*
@@ -98,30 +157,11 @@ class func_to_integrate: public RcppNumerical::Func
   for(i in 1:length(t))
   {
   time = t[i]; 
-  m[i] = splicing.k*exp(-Gamma(t = Tstable+time, gamma = gamma, eps.gamma=eps.gamma, phase.gamma=phase.gamma)) *integrate(f2integrate, lower = 0, upper = Tstable+time, par = c(gamma, eps.gamma, phase.gamma, Min, Amp, phase, beta))$value
+  m[i] = splicing.k*exp(-Gamma(t = Tstable+time, gamma = gamma, eps.gamma=eps.gamma, phase.gamma=phase.gamma)) *
+ integrate(f2integrate, lower = 0, upper = Tstable+time, par = c(gamma, eps.gamma, phase.gamma, Min, Amp, phase, beta))$value
   }
   return(m)
   }
-  f2integrate = function(t, par)
-  {
-  gamma = par[1]; 
-  eps.gamma= par[2];
-  phase.gamma= par[3]; 
-  Min = par[4];
-  Amp = par[5];
-  phase = par[6];
-  beta = par[7];
-  return(exp(Gamma(t, gamma, eps.gamma, phase.gamma)) * compute.s.beta(t, Min, Amp, phase, beta))
-  }
-#### This is the integral of degradation function
-  Gamma = function(t = 0, gamma = log(2)/3, eps.gamma = 0.2, phase.gamma = 0)
-  {
-  w = 2*pi/24;
-  Gamma = gamma*(t + eps.gamma/w * sin(w*(t-phase.gamma)))
-#Gamma = gamma*(t+ eps.gamma/w * sin(w*(t-phase.gamma)))
-#Gamma = gamma*(t+ eps.gamma/w * sin(w*(t-phase.gamma)))
-#Gamma = gamma*t + amp.gamma/2*(t+ sin(w*(t-phase.gamma))/w)
-  return(Gamma)
   }
  * 
  */
